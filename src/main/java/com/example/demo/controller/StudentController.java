cca4baa338f11c4c16aec851070699ac252a574d
bala-amypo <bala@amypo.in>
2025-12-18 06:42:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -27,6 +27,6 @@ public class StudentController{
     }
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable Long id){
-        return stdrepo.deleteData(id);
+        return stdser.deleteData(id);
     }
 }
